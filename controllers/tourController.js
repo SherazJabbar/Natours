34df4746610fe8998ffcd8cff6bbb8c79b481ece
Sheraz <sheraz.appelit@nextbridge.com>
2023-01-20 19:04:05 +0500

    Tour API Implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,13 +3,22 @@ const Tour = require("../models/tourModel");
 
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
+exports.aliasAllTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name, price.ratingsAverage, summary, difficulty';
+    next();
+}
 
-// 1) Route Handlers
-exports.getAllTours = async (req, res) => {
-    try {
-        console.log(req.query)
+class APIFeatures {
+    constructor(query, queryString) {
+        this.query = query;
+        this.queryStr = queryString;
+    };
+
+    filter() {
         // 1A) Filtering
-        const queryObj = { ...req.query };
+        const queryObj = { ...this.queryString };
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach(el => delete queryObj[el])
 
@@ -17,28 +26,92 @@ exports.getAllTours = async (req, res) => {
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-        let query = Tour.find(JSON.parse(queryStr));
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
+    sort() {
+        // 2) Sorting
+        if (this.queryString.sort) {
+            console.log(this.queryString)
+            const sortBy = this.queryString.sort.split(',').join(' ');
+            this.query = this.query.sort(sortBy);
+        } else {
+            this.query = this.query.sort('createdAt');
+        }
+        return this;
+    }
+
+    limitFields() {
+        //3) field limiting
+        if (this.queryString.fields) {
+            const fields = this.queryString.fields.split(',').join(' ');
+            this.query = this.query.select(fields);
+        } else {
+            this.query = this.query.select('-__v');
+        }
+        return this;
+    }
+
+    paginate() {
+        // 4) Pagination
+        const page = this.queryString.page * 1 || 1;
+        const limit = this.queryString.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit);
+    }
+}
+
+// 1) Route Handlers
+exports.getAllTours = async (req, res) => {
+    try {
+        console.log(req.query)
+        // 1A) Filtering
+        // const queryObj = { ...req.query };
+        // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        // excludedFields.forEach(el => delete queryObj[el])
+
+        // // 1B) Advanced Filtering
+        // console.log(queryObj);
+        // let queryStr = JSON.stringify(queryObj);
+        // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        // let query = Tour.find(JSON.parse(queryStr));
         // const query = Tour.find().where('duration').equals(5).where('difficulty').equals('easy')
 
         // 2) Sorting
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            console.log("sortBy", sortBy);
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('createdAt');
-        }
+        // if (req.query.sort) {
+        //     const sortBy = req.query.sort.split(',').join(' ');
+        //     query = query.sort(sortBy);
+        // } else {
+        //     query = query.sort('createdAt');
+        // }
 
         //3) field limiting
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        } else {
-            query = query.select('-__v');
-        }
+        // if (req.query.fields) {
+        //     const fields = req.query.fields.split(',').join(' ');
+        //     query = query.select(fields);
+        // } else {
+        //     query = query.select('-__v');
+        // }
+
+        // 4) Pagination
+        // const page = req.query.page * 1 || 1;
+        // const limit = req.query.limit * 1 || 100;
+        // const skip = (page - 1) * limit;
+
+        // query = query.skip(skip).limit(limit);
+        // if (req.query.page) {
+        //     // return number of documents
+        //     const numTours = await Tour.countDocuments();
+        //     if (skip >= numTours) throw new Error('This page doesnot exists')
+
+        // }
 
 
-        const tours = await query;
+        // Execute Query
+        const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+        const tours = await features.query;
 
         return res.status(200).json({
             status: 'success',
@@ -48,7 +121,8 @@ exports.getAllTours = async (req, res) => {
             }
         })
     } catch (err) {
-        return res.send(404).json({
+        console.log(err)
+        return res.status(404).json({
             status: "Failed",
             message: err
         })
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,11 +2,12 @@ const express = require('express')
 const tourController = require('../controllers/tourController');
 const router = express.Router();
 
-// router.param('id', tourController)
+
+router.route('/top-5-cheap').get(tourController.aliasAllTours, tourController.getAllTours)
 
 // chaining middlewares
 router.route(`/`).get(tourController.getAllTours).post(tourController.createTour);
 router.route(`/:id`).get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
- 
+
 module.exports = router;
