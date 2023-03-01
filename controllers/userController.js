f310c4f18c77fb625996dc6ce4d99cd5738c3184
Sheraz <sheraz.appelit@nextbridge.com>
2023-03-01 20:43:11 +0500

    Auth controller implemented

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,38 @@
-exports.getAllUsers = (req,res) => {
+const { query } = require("express");
+const User = require("../models/userModal");
+const catchAsync = require("../utils/catchAsync");
+
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+
+    const users = await User.find();
+    return res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users: users
+        }
+    })
+});
+exports.createUser = (req, res) => {
     res.status(500).json({
         status: 'error',
         message: "Route not implemented"
     })
 }
-exports.createUser = (req,res) => {
+exports.getUser = (req, res) => {
     res.status(500).json({
         status: 'error',
         message: "Route not implemented"
     })
 }
-exports.getUser = (req,res) => {
+exports.updateUser = (req, res) => {
     res.status(500).json({
         status: 'error',
         message: "Route not implemented"
     })
 }
-exports.updateUser = (req,res) => {
-    res.status(500).json({
-        status: 'error',
-        message: "Route not implemented"
-    })
-}
-exports.deleteUser = (req,res) => {
+exports.deleteUser = (req, res) => {
     res.status(500).json({
         status: 'error',
         message: "Route not implemented"
--- models/userModal.js
+++ models/userModal.js
@@ -18,7 +18,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please provide a password'],
-        minLength: 8
+        minLength: 8,
+        select: false
     },
     passwordConfirm: {
         type: String,
@@ -45,6 +46,11 @@ userSchema.pre('save', async function (next) {
     this.passwordConfirm = undefined;
     next();
 });
+
+// Instance Method : available on all documents of a same collection 
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model('User', userSchema);
 
